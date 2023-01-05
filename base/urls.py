15b853dc03ad5d6495adb272ee90c32c72522656
Luke Climenhage <luke.climenhage@gmail.com>
2023-01-05 15:46:34 -0500

    Added ability to create new rooms using form

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('chatroom/<str:primaryKey>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name="create-room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -13,6 +14,17 @@ def room(request, primaryKey):
     currentRoom = Room.objects.get(id=primaryKey)
     
     context = {'room':currentRoom}
-
-
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        # Save the form data in the database if it's valid
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
